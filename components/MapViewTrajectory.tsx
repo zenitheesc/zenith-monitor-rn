54c2d562a379f666615c8dd9f0f5d2b14453d26d
Italo <italomatos764@gmail.com>
2025-02-10 17:53:12 -0300

    enhancement: enhance trajectory visualization and simplify region handling in MapViewTrajectory

--- components/MapViewTrajectory.tsx
+++ components/MapViewTrajectory.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Coordinates } from '@/types/types';
 import { StyleSheet } from 'react-native';
 import MapView, { Polyline, Marker } from 'react-native-maps';
 
-export default function MapViewTrajectory({ coordinates, region }: { coordinates: Coordinates[], region: any }) {
+export default function MapViewTrajectory({ coordinates }: { coordinates: Coordinates[] }) {
+    const teste = [coordinates[0], coordinates[coordinates.length - 1]];
+    let mapRef = useRef<MapView>(null);
+
+    useEffect(() => {
+            mapRef.current?.fitToCoordinates(teste, {
+                edgePadding: { top: 50, right: 100, bottom: 50, left: 100 },
+                animated: true,
+            });
+    }, [coordinates]);
+
     return (
-        <MapView
+        <>
+        {coordinates.length && 
+        (
+            <MapView
             style={styles.map}
-            region={region}
+            ref={mapRef}
+            onMapReady={() => {
+                if (mapRef.current) {
+                    mapRef.current.fitToCoordinates([coordinates[0], coordinates[coordinates.length - 1]], {
+                        edgePadding: { top: 50, right: 100, bottom: 50, left: 100 },
+                        animated: true,
+                    });
+                }
+            }}
         >
+            
             <Polyline
                 coordinates={coordinates}
                 strokeColor="#9449de"
                 strokeWidth={2}
+                
             />
             <Marker
                 coordinate={coordinates[0]}
@@ -27,6 +50,11 @@ export default function MapViewTrajectory({ coordinates, region }: { coordinates
                 image={require('../assets/images/balloon-end-mission-icon.png')}
             />
         </MapView>
+        )
+        }
+        
+        </>
+        
     );
 }
 
--- screens/Missions/TrajectoryScreen.tsx
+++ screens/Missions/TrajectoryScreen.tsx
@@ -1,45 +1,23 @@
 import MapViewTrajectory from '@/components/MapViewTrajectory';
-import { RegionDataApi } from '@/types/types';
+import { Coordinates, RegionDataApi } from '@/types/types';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Appbar } from 'react-native-paper';
 
-const regionSPDefault: RegionDataApi = {
-    latitude: -23.3282,
-    longitude: -51.13811,
-    latitudeDelta: 0.5,
-    longitudeDelta: 0.5,
-}
-
 export default function TrajectoryScreen({ navigation, route }: { navigation: any, route: any }) {
     const { jsonUrl } = route.params;
-    const [coordinates, setCoordinates] = useState([]);
-    const [region, setRegion] = useState({
-        latitude: -23.3282,
-        longitude: -51.13811,
-        latitudeDelta: 0.5,
-        longitudeDelta: 0.5,
-    });
+    const [coordinates, setCoordinates] = useState<Coordinates[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     const loadTrajectoryData = async () => {
         try {
             const response = await fetch(jsonUrl);
             const data = await response.json();
-            const trajectory = data.map((item: any) => ({
+            const trajectory = data?.map((item: any) => ({
                 latitude: item?.lat,
                 longitude: item?.lon,
             }));
-            setCoordinates(trajectory);
-
-            if (trajectory.length > 0) {
-                setRegion({
-                    latitude: trajectory[0].latitude,
-                    longitude: trajectory[0].longitude,
-                    latitudeDelta: 0.5,
-                    longitudeDelta: 0.5,
-                });
-            }
+            setCoordinates(trajectory);            
         } catch (error) {
             console.error('Erro ao buscar dados de trajetória', error);
         } finally {
@@ -58,7 +36,7 @@ export default function TrajectoryScreen({ navigation, route }: { navigation: an
                 <Appbar.Content title="Missões" />
             </Appbar.Header>
             { loading && <ActivityIndicator size="large" color="#F8BD00" /> }
-            {!loading && <MapViewTrajectory coordinates={coordinates} region={region} />}
+            {!loading && coordinates?.length > 1 && <MapViewTrajectory coordinates={coordinates} />}
         </View>
     );
 };
