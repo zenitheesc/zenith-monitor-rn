6a034e198d77aeac119692bc5baf5d0f932baadf
Italo <italomatos764@gmail.com>
2025-03-03 23:37:52 -0300

    add date formatting and conversion utilities; enhance MissionsScreen with progress steps

--- screens/Missions/MissionsScreen.tsx
+++ screens/Missions/MissionsScreen.tsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
-import { ActivityIndicator, Appbar, Avatar, Card, Icon } from 'react-native-paper';
-import { vw } from '../../utils/utils';
+import {
+    ActivityIndicator,
+    Appbar,
+    Avatar,
+    Card,
+    Chip,
+    Icon,
+    IconButton,
+} from 'react-native-paper';
+import { vw, formatDateToReadableString, convertMeterToKilometer } from '../../utils/utils';
 import { getAllMissionsSummary } from '@/services/MissionsSummaryApi';
 import { MissionSummary } from '@/types/types';
+import ProgressStepV2 from '../../components/ProgressStepV2';
 
-export default function MissionsScreen({ navigation, route }: { navigation: any, route: any }) {
+export default function MissionsScreen({ navigation, route }: { navigation: any; route: any }) {
     const [loading, setLoading] = useState<boolean>(true);
     const [missions, setMissions] = useState<MissionSummary[]>([]);
 
@@ -32,30 +41,79 @@ export default function MissionsScreen({ navigation, route }: { navigation: any,
             <ScrollView contentContainerStyle={styles.cardsContainer}>
                 {loading && <ActivityIndicator size="large" color="#F8BD00" />}
 
-                {!loading && missions?.map((mission, index) => (
-                    <Card
-                        key={index}
-                        style={styles.card}
-                        onPress={() => navigation.navigate("TrajectoryScreen", { jsonUrl: mission?.download_url })}
-                        elevation={1}
-                    >
-                        <Card.Title
-                            title={`Missão ${index + 1} - ${mission.launch_datetime}`}
-                            subtitle={`Cidade: ${mission.launch_city}\nAltitude Máxima: ${mission.max_altitude.toFixed(2)} m`}
-                            subtitleNumberOfLines={2}
-                            titleNumberOfLines={1}
-                            left={(props) => <Avatar.Icon {...props} icon="map-marker-radius" />}
-                            right={(props) => <Icon {...props} source="chevron-right" />}
-                            style={styles.cardTitle}
-                        />
-                    </Card>
-                ))}
+                {!loading &&
+                    missions?.map((mission, index) => (
+                        <Card
+                            key={index}
+                            style={styles.card}
+                            onPress={() =>
+                                navigation.navigate('TrajectoryScreen', {
+                                    jsonUrl: mission?.download_url,
+                                })
+                            }
+                            elevation={1}
+                        >
+                            <Card.Title
+                                title={`Missão ${index + 1}`}
+                                subtitle={formatDateToReadableString(mission.launch_datetime)}
+                                left={(props) => <Avatar.Icon {...props} icon="airballoon" />}
+                                right={(props) => (
+                                    <IconButton
+                                        {...props}
+                                        icon="dots-vertical"
+                                        size={30}
+                                        style={{ marginTop: -25 }}
+                                        onPress={() => console.log('Pressed')}
+                                    />
+                                )}
+                                style={styles.cardTitle}
+                            />
+                            <Card.Content>
+                                <View
+                                    style={{
+                                        flexDirection: 'row',
+                                        flex: 1,
+                                        width: '100%',
+                                        alignContent: 'space-between',
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            maxHeight: 'auto',
+                                            width: '50%',
+                                        }}
+                                    >
+                                        <ProgressStepV2
+                                            key={`step-indicator-${index}`}
+                                            steps={[
+                                                {
+                                                    name: mission.launch_city,
+                                                    icon: 'map-marker',
+                                                },
+                                                {
+                                                    name: mission.launch_city,
+                                                    icon: 'map-marker-radius',
+                                                },
+                                            ]}
+                                        />
+                                    </View>
+                                    <View>
+                                        <Chip icon="arrow-up-down" style={{ borderRadius: 25 }}>
+                                            {`${convertMeterToKilometer(
+                                                mission.max_altitude
+                                            )} km\n`}
+                                        </Chip>
+                                    </View>
+                                </View>
+                            </Card.Content>
+                        </Card>
+                    ))}
 
-                {!loading && missions.length === 0 &&
+                {!loading && missions.length === 0 && (
                     <Card style={styles.card}>
                         <Card.Title title="Nenhuma missão encontrada" />
                     </Card>
-                }
+                )}
             </ScrollView>
         </View>
     );
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,24 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { Dimensions } from 'react-native';
 
 export const vh = (heightValue: number) => (heightValue * Dimensions.get('window').height) / 100;
 export const vw = (widthValue: number) => (widthValue * Dimensions.get('window').width) / 100;
+
+export const formatDateToReadableString = (dateInfo: string) => {
+    const date = new Date(dateInfo);
+    const currentYear = new Date().getFullYear();
+    const dateYear = date.getFullYear();
+
+    return format(
+        date,
+        dateYear === currentYear ? 'EEEEEE., dd MMM' : "EEEEEE., dd MMM 'de' yyyy",
+        {
+            locale: ptBR,
+        }
+    );
+};
+
+export const convertMeterToKilometer = (meters: number) => {
+    return (meters / 1000).toFixed(2);
+};
